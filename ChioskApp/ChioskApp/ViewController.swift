b1b648d6a62f31b2f3331523d95324c0ef0e188b
DoyleHWorks <DoyleHWorks@gmail.com>
2024-11-25 11:11:25 +0900

    🎉 [INIT] Create Xcode project files

---
+++ ChioskApp/ChioskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChioskApp
+//
+//  Created by t0000-m0112 on 2024-11-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
