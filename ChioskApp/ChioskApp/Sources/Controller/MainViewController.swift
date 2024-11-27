c9ed15ce9c8cf00a70460d3589c6d64e790ea962
DoyleHWorks <DoyleHWorks@gmail.com>
2024-11-27 11:14:14 +0900

    🧱 [REFACTOR] MenuListNavigator 인스턴스 추가
    MainViewController에 MenuListNavigatorVC 하위 뷰 추가

--- ChioskApp/ChioskApp/Sources/Controller/MainViewController.swift
+++ ChioskApp/ChioskApp/Sources/Controller/MainViewController.swift
@@ -12,10 +12,30 @@ import Then
 
 class MainViewController: UIViewController {
 
+    let menuListNavigator = MenuListNavigatorVC()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupConstraints()
     }
-    
+
+    // UI 설정
+    private func setupUI() {
+        // menuListNavigator를 하위 뷰로 추가
+        addChild(menuListNavigator)
+        view.addSubview(menuListNavigator.view)
+        menuListNavigator.didMove(toParent: self)
+    }
+
+    // 제약 조건 설정
+    private func setupConstraints() {
+        menuListNavigator.view.snp.makeConstraints { make in
+            make.edges.equalToSuperview() // 전체 화면에 꽉 차게
+        }
+    }
 }
 
+#Preview("MainViewController") {
+    MainViewController()
+}
