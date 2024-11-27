72b5a5343661f03c106f5309c37d54d55c65954b
kangminseoung <alstjd2356@gmail.com>
2024-11-27 20:34:21 +0900

    ✨[FEAT] UICollectionViewCell에 컨테이너 뷰 기능 구현 추가

--- ChioskApp/ChioskApp/Sources/View/OrderSummaryView.swift
+++ ChioskApp/ChioskApp/Sources/View/OrderSummaryView.swift
@@ -9,200 +9,186 @@
 import SnapKit
 import UIKit
 
-let cellID = "OrderCell"
+let cellID = "OrderCell" // 컬렉션 뷰 셀의 재사용을 위한 식별자 설정
 
 class OrderSummaryView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    
+
+    // 섹션당 아이템 수 설정
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return 2 // 2개의 이미지 반환
     }
     
+    // 셀 구성 메서드
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // 컬렉션 뷰에서 재사용 가능한 셀을 가져온다.
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! OrderSummaryViewCell
-        
-        cell.backgroundColor = .white // 셀의 배경색을 빨간색으로 지정.
-        
-        return cell
+        return cell // 구성된 셀 반환
     }
+
+    private var entireView = UIView() // 주문 하단 전체 뷰
     
-
-    // 주문 하단 전체 뷰
-    private var entireView = UIView()
-
-    // 주문 내역 관한 이미지 및 레이블
+    // 주문 내역 데이터 배열 생성
     private var orderList = [
         (
-            image: UIImage(named: "chicken1"), name: "후라이드1", price: "25,000원",
-            quantity: 1
+            image: UIImage(named: "chicken1"), // 첫번째 치킨 이미지
+            name: "후라이드1", // 첫번째 치킨 이름
+            price: "25,000원", // 첫번째 치킨 가격
+            quantity: 1 // 첫번째 치킨 초기 수량
         ),
         (
-            image: UIImage(named: "chicken2"), name: "후라이드2", price: "25,000원",
-            quantity: 1
+            image: UIImage(named: "chicken2"), // 두번째 치킨 이미지
+            name: "후라이드2", // 두번째 치킨 이름
+            price: "25,000원", // 두번째 치킨 가격
+            quantity: 1 // 두번째 치킨 초기 수량
         ),
     ]
-
-    // 총 수량 및 총 금액 레이블
-    private let itemQuantityLabel = UILabel()  // 수량 레이블
-    private let totalAmountLabel = UILabel()  // 총액 레이블
+    
+    // 레이블 정의
+    private let itemQuantityLabel = UILabel()  // 총 수량 표시
+    private let totalAmountLabel = UILabel()  // 합계 레이블
     private let amountValueLabel = UILabel()  // 금액 표시 레이블
-
-    // 직원 호출 버튼, 취소 버튼, 결제 버튼
+    
+    // 버튼 정의
     private var employeeCallButton = UIButton()  // 직원 호출 버튼
     private var cancelButton = UIButton()  // 취소 버튼
     private var paymentButton = UIButton()  // 결제 버튼
-
-    // 주문 내역 CollectionView
+    
+    // 컬렉션 뷰 정의
     let collectionView: UICollectionView = {
-        let flowlayout = UICollectionViewFlowLayout()
+        let flowlayout = UICollectionViewFlowLayout() // 레이아웃 정의
         let cv = UICollectionView(
             frame: .zero, collectionViewLayout: flowlayout)
-        flowlayout.scrollDirection = .vertical // 가로 스크롤
-        flowlayout.itemSize = CGSize(width: 160, height: 160) // 셀 크기
-        flowlayout.minimumLineSpacing = 9 // 셀 간의 간격 9
+        flowlayout.scrollDirection = .vertical // 세로 스크롤
+        flowlayout.itemSize = CGSize(width: 160, height: 160) // 셀 크기 설정
+        flowlayout.minimumLineSpacing = 9 // 셀 간의 간격 설정
         return cv
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-        setupButtons()
+        setupUI() // UI 설정
+        setupButtons() // 버튼 설정
         
-        // delegate와 dataSource 설정.
+        // 델리게이트 및 데이터소스 설정
         collectionView.delegate = self
         collectionView.dataSource = self
-
     }
     
     private func setupUI() {
-        // 전체 배경색
-        view.backgroundColor = .white
-
-        // 하단 회색 바탕의 전체 뷰
-        entireView.backgroundColor = .systemGray6  // 주문 하단 전체 뷰 배경색은 시스템 회색6
-        entireView.layer.cornerRadius = 10  // 모서리 둥글게 처리
-        view.addSubview(entireView)  // 뷰에 주문 하단 전체 뷰를 추가하여 화면에 표시되도록 설정.
-
-        // entireView 레이아웃 설정
+        
+        view.backgroundColor = .white // 기본 배경을 흰색으로 설정
+    
+        // 하단 전체 뷰 설정
+        entireView.backgroundColor = .systemGray6 // 배경색 시스템 회색
+        entireView.layer.cornerRadius = 10 // 모서리를 둥글게 설정
+        view.addSubview(entireView) // 뷰에 추가하여 표시
+        
+        // 하단 전체 뷰 레이아웃
         entireView.snp.makeConstraints {
-            $0.leading.trailing.bottom.equalToSuperview()  // 좌우 화면 꽉 채움
-            $0.height.equalTo(326)  // 높이는 326으로 설정.
+            $0.leading.trailing.bottom.equalToSuperview() // 좌우 및 하단 고정
+            $0.height.equalTo(326) // 높이는 326으로 설정
         }
         
-        // collectionView 속성 설정
-        collectionView.backgroundColor = .systemGray6
-//        collectionView.layer.cornerRadius = 12
- 
-          // entireView에 collectionView 추가하여 화면에 표시.
-        entireView.addSubview(collectionView)
+        // 컬렉션 뷰 설정
+        collectionView.backgroundColor = .systemGray6 // 컬렉션 뷰 배경색 시스템 회색
+        entireView.addSubview(collectionView) // 컬렉션 뷰를 하단 전체 뷰에 추가
         
-        // collectionView 레이아웃 설정.
+        // 컬렉션 뷰 레이아웃 설정
         collectionView.snp.makeConstraints {
-            $0.bottom.equalToSuperview().inset(100)
-            $0.top.equalToSuperview().inset(59)
-            $0.leading.trailing.equalToSuperview().inset(33)
+            $0.bottom.equalToSuperview().inset(100) // 하단 간격 100
+            $0.top.equalToSuperview().inset(59) // 상단 간격 59
+            $0.leading.trailing.equalToSuperview().inset(33) // 좌우 간격 33
         }
         
+        // 셀 등록
         collectionView.register(OrderSummaryViewCell.self, forCellWithReuseIdentifier: cellID)
         
-        // itemQuantityLabel 속성 설정.
-        itemQuantityLabel.text = "0개"  // 임시 초기값
-        itemQuantityLabel.textColor = .black
-        itemQuantityLabel.font = .boldSystemFont(ofSize: 25)
-
-        // entireView에 수량 레이블 추가하여 화면에 표시되도록 설정.
-        entireView.addSubview(itemQuantityLabel)
-
-        // itemQuantityLabel 레이아웃 설정.
+        // 수량 레이블 설정
+        itemQuantityLabel.text = "0개" // 초기 텍스트 설정
+        itemQuantityLabel.textColor = .black // 텍스트 색상은 검정색 설정
+        itemQuantityLabel.font = .boldSystemFont(ofSize: 25) // 폰트 사이즈 25, 폰트 타입은 시스템 폰트
+        entireView.addSubview(itemQuantityLabel) // 수량 레이블 추가
+        
+        // 수량 레이블 레이아웃
         itemQuantityLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(31)
-            $0.top.equalToSuperview().inset(20)
+            $0.leading.equalToSuperview().inset(31) // 왼쪽 간격 31
+            $0.top.equalToSuperview().inset(20) // 위쪽 간격 20
         }
-
-        // totalAmountLabel 속성 설정.
-        totalAmountLabel.text = "합계"
-        totalAmountLabel.textColor = .black
-        totalAmountLabel.font = .boldSystemFont(ofSize: 25)
-
-        // entireView에 totalAmountLabel을 추가하여 화면에 표시되도록 설정.
-        entireView.addSubview(totalAmountLabel)
-
-        // totalAmountLabel 레이아웃 설정.
+        
+        // 합계 레이블 설정
+        totalAmountLabel.text = "합계" // 초기 텍스트 설정
+        totalAmountLabel.textColor = .black // 텍스트 색상은 검정색 설정
+        totalAmountLabel.font = .boldSystemFont(ofSize: 25) // 폰트 사이즈 25, 폰트 타입은 시스템 폰트
+        entireView.addSubview(totalAmountLabel) // 합계 레이블 추가
+        
+        // 합계 레이블 레이아웃
         totalAmountLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(196)
-            $0.top.equalToSuperview().inset(20)
+            $0.leading.equalToSuperview().inset(196) // 왼쪽에서 간격 196
+            $0.top.equalToSuperview().inset(20) // 위쪽에서 간격 20
         }
-
-        // amountValueLabel 속성 설정
-        amountValueLabel.text = "50,000원"  // 임시 초기값
-        amountValueLabel.textColor = .red
-        amountValueLabel.font = .boldSystemFont(ofSize: 25)
-        entireView.addSubview(amountValueLabel)
-
-        // amountValueLabel 레이아웃 설정
+        
+        // 금액 표시 레이블 설정
+        amountValueLabel.text = "50,000원" // 초기 금액 설정
+        amountValueLabel.textColor = .chioskPrimary // 텍스트 색상은 chioskPrimary 설정
+        amountValueLabel.font = .boldSystemFont(ofSize: 25) // 폰트 사이즈 25, 폰트 타입은 시스템 폰트
+        entireView.addSubview(amountValueLabel) // 금액 레이블 추가
+        
+        // 금액 표시 레이블 레이아웃
         amountValueLabel.snp.makeConstraints {
-            $0.trailing.equalToSuperview().offset(-30)  // 합계 레이블 옆에 배치
-            $0.centerY.equalTo(totalAmountLabel)  // 수직 정렬
-
+            $0.trailing.equalToSuperview().offset(-30) // 오른쪽에서 간격 30
+            $0.centerY.equalTo(totalAmountLabel) // 합계 레이블과 세로 중앙 정렬
         }
     }
-
+    
     private func setupButtons() {
-
-        // employeeCallButton 속성 설정.
-        employeeCallButton.setTitle("직원호출", for: .normal)
-        employeeCallButton.backgroundColor = .systemGray4
-        employeeCallButton.layer.cornerRadius = 10
-        employeeCallButton.setTitleColor(.black, for: .normal)
-        employeeCallButton.titleLabel?.font = .boldSystemFont(ofSize: 16)
-
-        // entireView에 employeeCallButton을 추가하여 화면에 표시되도록 설정.
-        entireView.addSubview(employeeCallButton)
-
-        // employeeCallButton 레이아웃 설정.
+        // 직원 호출 버튼 설정
+        employeeCallButton.setTitle("직원호출", for: .normal) // 텍스트 설정
+        employeeCallButton.backgroundColor = .systemGray4 // 배경색 설정
+        employeeCallButton.layer.cornerRadius = 10 // 모서리 10만큼 둥글게 설정
+        employeeCallButton.setTitleColor(.black, for: .normal) // 텍스트 색상은 검정색 설정
+        employeeCallButton.titleLabel?.font = .boldSystemFont(ofSize: 16) // 폰트 사이즈 16, 폰트 타입은 시스템 폰트
+        entireView.addSubview(employeeCallButton) // 직원호출 버튼 추가
+        
+        // 직원 호출 버튼 레이아웃
         employeeCallButton.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(33)
-            $0.bottom.equalToSuperview().inset(48)
-            $0.width.equalTo(100)
-            $0.height.equalTo(50)
+            $0.leading.equalToSuperview().inset(33) // 왼쪽 간격 33
+            $0.bottom.equalToSuperview().inset(48) // 아래쪽에서 간격 48
+            $0.width.equalTo(100) // 버튼 너비 100
+            $0.height.equalTo(50) // 버튼 높이 50
         }
-
-        // cancelButton 속성 설정.
-        cancelButton.setTitle("취소하기", for: .normal)
-        cancelButton.backgroundColor = .systemGray4
-        cancelButton.layer.cornerRadius = 10
-        cancelButton.setTitleColor(.black, for: .normal)
-        cancelButton.titleLabel?.font = .boldSystemFont(ofSize: 16)
-        // entireView에 cancelButton을 추가하여 화면에 표시되도록 설정.
-        entireView.addSubview(cancelButton)
-
-        // cancelButton 레이아웃 설정.
+        
+        // 취소 버튼 설정
+        cancelButton.setTitle("취소하기", for: .normal) // 텍스트 설정
+        cancelButton.backgroundColor = .systemGray4 // 배경색 회색 설정
+        cancelButton.layer.cornerRadius = 10 // 모서리 10만큼 둥글게 설정
+        cancelButton.setTitleColor(.black, for: .normal) // 텍스트 색상 검정색 설정
+        cancelButton.titleLabel?.font = .boldSystemFont(ofSize: 16) // 폰트 사이즈 16, 폰트 타입은 시스템 폰트
+        entireView.addSubview(cancelButton) // 취소하기 버튼 추가
+        
+        // 취소 버튼 레이아웃
         cancelButton.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(48)
-            $0.width.equalTo(100)
-            $0.height.equalTo(50)
+            $0.centerX.equalToSuperview() // 수평 중앙 정렬
+            $0.bottom.equalToSuperview().inset(48) // 아래쪽에서 간격 48
+            $0.width.equalTo(100) // 버튼 너비 100
+            $0.height.equalTo(50) // 버튼 높이 50
         }
-
-        // paymentButton 속성 설정.
-        paymentButton.setTitle("결제하기", for: .normal)
-        paymentButton.backgroundColor = .systemRed
-        paymentButton.layer.cornerRadius = 10
-        paymentButton.setTitleColor(.white, for: .normal)
-        paymentButton.titleLabel?.font = .boldSystemFont(ofSize: 16)
-
-        // entireView에 paymentButton을 추가하여 화면에 표시되도록 설정.
-        entireView.addSubview(paymentButton)
-
-        // paymentButton 레이아웃 설정.
+        
+        // 결제 버튼 설정
+        paymentButton.setTitle("결제하기", for: .normal) // 텍스트 설정
+        paymentButton.backgroundColor = .systemRed // 배경색 빨간색으로 설정.
+        paymentButton.layer.cornerRadius = 10 // 모서리는 10만큼 둥글게 설정
+        paymentButton.setTitleColor(.white, for: .normal) // 텍스트 색상은 하얀색 설정
+        paymentButton.titleLabel?.font = .boldSystemFont(ofSize: 16) // 폰트 사이즈는 16, 폰트 타입은 시스템 폰트
+        entireView.addSubview(paymentButton) // 결제하기 버튼 추가
+        
+        // 결제 버튼 레이아웃
         paymentButton.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(33)
-            $0.bottom.equalToSuperview().inset(48)
-            $0.width.equalTo(100)
-            $0.height.equalTo(50)
-
+            $0.trailing.equalToSuperview().inset(33) // 오른쪽에서 간격 33
+            $0.bottom.equalToSuperview().inset(48) // 아래쪽에서 간격 48
+            $0.width.equalTo(100) // 버튼의 너비 100
+            $0.height.equalTo(50) // 버튼의 높이 50
         }
     }
-
 }
 
 #Preview("MenuSelectionHandler") {
--- ChioskApp/ChioskApp/Sources/View/OrderSummaryViewCell.swift
+++ ChioskApp/ChioskApp/Sources/View/OrderSummaryViewCell.swift
@@ -9,110 +9,124 @@ import UIKit
 
 class OrderSummaryViewCell: UICollectionViewCell {
     
-    private let chickenImageView = UIImageView()
-    private let goodsLabel = UILabel() // 상품명 레이블
-    private let priceLabel = UILabel() // 가격 레이블
-    private let quantityLabel = UILabel() // 수량 표시
-    private let minusButton = UIButton() // - 버튼
-    private let plusButton = UIButton() // + 버튼
+    private let containerView = UIView() // 셀 전체 영역을 감싸는 컨테이너 뷰
+    private let chickenImageView = UIImageView() // 치킨 이미지 표시할 뷰
+    private let goodsLabel = UILabel() // 상품명 표시하는 레이블
+    private let priceLabel = UILabel() // 가격 표시하는 레이블
+    private let quantityLabel = UILabel() // 수량 표시하는 레이블
+    private let minusButton = UIButton() // 수량 감소 버튼
+    private let plusButton = UIButton() // 수량 증가 버튼
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupCellUI()
+        setupCellUI() // 셀 UI 설정 메서드 호출
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been implemented") // 서브 클래싱할 때 반드시 구현해야 하는 초기화 메서드 중 한다.
     }
     
     private func setupCellUI() {
         
-        contentView.backgroundColor = .white
-        contentView.layer.cornerRadius = 8 // 모서리 반경 설정.
-        contentView.layer.borderWidth = 1
-        contentView.layer.borderColor = UIColor.systemGray4.cgColor
+        // 컨테이너 뷰 설정
+        containerView.backgroundColor = .white // 배경색 하얀색으로 설정
+        containerView.layer.cornerRadius = 8 // 컨테이너의 모시러 둥글게 설정
+        containerView.layer.borderWidth = 1 // 테두리 두께 설정
+        containerView.layer.borderColor = UIColor.systemGray4.cgColor // 테두리 색상 설정
+        contentView.addSubview(containerView) // 컨테이너 뷰를 셀에 추가
         
+        // 컨테이너 뷰 레이아웃 설정
+        containerView.snp.makeConstraints {
+            $0.edges.equalToSuperview() // 셀의 전체 영역을 채우도록 설정
+        }
         
-        chickenImageView.image = UIImage(named: "Fried")
-        chickenImageView.backgroundColor = .white
-        chickenImageView.contentMode = .scaleAspectFit
+        // 치킨 이미지 뷰 설정
+        chickenImageView.image = UIImage(named: "Chickens_Fried") // 기본 이미지 설정
+        chickenImageView.backgroundColor = .white // 배경색 흰색
+        chickenImageView.contentMode = .scaleAspectFit // 비율 유지
+        contentView.addSubview(chickenImageView) // 컨테이너 뷰에 이미지 뷰 추가
         
-        contentView.addSubview(chickenImageView)
-        
+        // 이미지 뷰 레이아웃 설정
         chickenImageView.snp.makeConstraints{
-            $0.top.equalToSuperview().inset(12) // 위쪽에서 떨어진 위치
-            $0.bottom.equalToSuperview().inset(80) // 아래쪽에서 떨어진 위치
-            $0.centerX.equalToSuperview() // 부모 뷰의 가로 중심 정렬.
-            $0.width.equalTo(65) // 뷰 가로 크기
-            $0.height.equalTo(77) // 뷰 세로 크기
+            $0.top.equalToSuperview().inset(12) // 위쪽에서 12 간격
+            $0.centerX.equalToSuperview() // 가로 중심 정렬
+            $0.width.equalTo(65) // 가로 크기
+            $0.height.equalTo(77) // 세로 크기
 
         }
         
         // 상품명 레이블 설정
         goodsLabel.text = "후라이드1" // 임시 텍스트
-        goodsLabel.font = .boldSystemFont(ofSize: 10)
-        goodsLabel.textColor = .black
-        goodsLabel.textAlignment = .center
+        goodsLabel.font = .boldSystemFont(ofSize: 10) // 폰트 사이즈 10, 폰트 타입 시스템 폰트
+        goodsLabel.textColor = .black // 텍스트 색상 검정색
+        goodsLabel.textAlignment = .center // 중앙 정렬
+        contentView.addSubview(goodsLabel) // 컨테이너 뷰에 상품명 레이블 추가
         
-        contentView.addSubview(goodsLabel)
-        
+        // 상품명 레이블 레이아웃 설정
         goodsLabel.snp.makeConstraints {
-            $0.top.equalTo(chickenImageView.snp.bottom).offset(12)
-            $0.leading.trailing.equalToSuperview().inset(5)
+            $0.top.equalTo(chickenImageView.snp.bottom).offset(12) // 이미지 하단 12 간격
+            $0.leading.trailing.equalToSuperview().inset(5) // 좌우 여백 5 간격
         }
 
         // 가격 레이블 설정
         priceLabel.text = "25,000원" // 임시 테스트
-        priceLabel.font = .systemFont(ofSize: 12, weight: .light)
-        priceLabel.textAlignment = .center
-        priceLabel.textColor = .chioskSecondaryColor
+        priceLabel.font = .systemFont(ofSize: 12, weight: .light) // 가벼운 폰트 설정
+        priceLabel.textAlignment = .center // 중앙 정렬
+        priceLabel.textColor = .chioskSecondaryColor // 텍스트 색상 설정
+        contentView.addSubview(priceLabel) // 컨테이너 뷰에 가격 레이블 추가
         
-        contentView.addSubview(priceLabel)
-        
+        // 가격 레이블 레이아웃 설정
         priceLabel.snp.makeConstraints {
-            $0.top.equalTo(goodsLabel.snp.bottom).offset(2)
-            $0.leading.trailing.equalToSuperview().inset(5)
+            $0.top.equalTo(goodsLabel.snp.bottom).offset(2) // 상품명 레이블 하단 2 포인트 간격
+            $0.leading.trailing.equalToSuperview().inset(5) // 좌우 여백 5 포인트
         }
         
-        // 수량 조절 버튼 설정
-        minusButton.setTitle("-", for: .normal)
-        minusButton.layer.borderWidth = 1
-        minusButton.layer.borderColor = UIColor.systemGray6.cgColor
-        minusButton.setTitleColor(.black, for: .normal)
-        minusButton.backgroundColor = .white
-        minusButton.layer.cornerRadius = 4
-        minusButton.titleLabel?.font = .systemFont(ofSize: 10)
+        // 수량 감소 버튼 설정
+        minusButton.setTitle("-", for: .normal) // 텍스트 - 설정
+        minusButton.layer.borderWidth = 1 // 테두리 두께를 1 설정
+        minusButton.layer.borderColor = UIColor.systemGray6.cgColor // 테두리 색상 설정
+        minusButton.setTitleColor(.black, for: .normal) // 텍스트 색상 설정
+        minusButton.backgroundColor = .white // 배경 흰색 설정
+        minusButton.layer.cornerRadius = 4 // 모서리 4 둥글게 설정
+        minusButton.titleLabel?.font = .systemFont(ofSize: 10) // 폰트 크기 설정
+        
+        // 수량 감소 버튼 레이아웃 설정
         minusButton.snp.makeConstraints {
-            $0.width.equalTo(20)
-            $0.height.equalTo(16)
+            $0.width.equalTo(20) // 버튼의 너비 20
+            $0.height.equalTo(16) // 버튼의 높이 16
         }
         
-        plusButton.setTitle("+", for: .normal)
-        plusButton.layer.borderWidth = 1
-        plusButton.layer.borderColor = UIColor.chioskSecondary.cgColor
-        plusButton.setTitleColor(.black, for: .normal)
-        plusButton.backgroundColor = .chioskSecondary
-        plusButton.layer.cornerRadius = 4
-        plusButton.titleLabel?.font = .systemFont(ofSize: 10)
+        // 수량 증가 버튼 설정
+        plusButton.setTitle("+", for: .normal) // 텍스트 설정
+        plusButton.layer.borderWidth = 1 // 테두리 두께 설정
+        plusButton.layer.borderColor = UIColor.chioskSecondary.cgColor // 테두리 색상 설정
+        plusButton.setTitleColor(.black, for: .normal) // 텍스트 색상 설정
+        plusButton.backgroundColor = .chioskSecondary // 배경색 설정
+        plusButton.layer.cornerRadius = 4 // 모서리 둥글게 설정
+        plusButton.titleLabel?.font = .systemFont(ofSize: 10) // 폰트 크기 설정
+        
+        // 수량 증가 버튼 레이아웃 설정
         plusButton.snp.makeConstraints {
-            $0.width.equalTo(20)
-            $0.height.equalTo(16)
+            $0.width.equalTo(20) // 버튼의 너비 20
+            $0.height.equalTo(16) // 버튼의 높이 16
         }
         
         // 수량 표시 레이블 설정
         quantityLabel.text = "0" // 임시 수량
-        quantityLabel.font = .systemFont(ofSize: 14)
-        quantityLabel.textAlignment = .center
+        quantityLabel.font = .systemFont(ofSize: 14) // 폰트 크기 설정
+        quantityLabel.textAlignment = .center // 중앙 정렬
 
-        // 수량 조절 버튼 및 레이블
-        let controlStack = UIStackView(arrangedSubviews: [minusButton, quantityLabel, plusButton])
-        controlStack.axis = .horizontal
-        controlStack.spacing = 7
-        controlStack.alignment = .center
-        contentView.addSubview(controlStack)
+        // 수량 조절 버튼 및 레이블 UIStackView 설정
+        let controlStack = UIStackView(arrangedSubviews: [minusButton, quantityLabel, plusButton]) // 버튼들과 레이블을 포함하는 스택뷰 생성
+        controlStack.axis = .horizontal // 수평 정렬
+        controlStack.spacing = 7 // 뷰 간 간격
+        controlStack.alignment = .center // 정렬 설정
+        contentView.addSubview(controlStack) // 컨테이너 뷰에 스택뷰 추가
+        
+        // 스택뷰 레이아웃 설정
         controlStack.snp.makeConstraints {
-            $0.top.equalTo(priceLabel.snp.bottom).offset(5)
-            $0.centerX.equalToSuperview()
+            $0.top.equalTo(priceLabel.snp.bottom).offset(5) // 가격 레이블 아래 5 포인트 간격
+            $0.centerX.equalToSuperview() // 가로 중심 정렬
         }
     }
 }
