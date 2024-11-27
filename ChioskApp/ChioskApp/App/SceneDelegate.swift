f403f6a5b1b04a17aecaab4cfe99347a0861887f
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-11-27 17:56:14 +0900

    🐛 [FIX] Update SceneDelegate.swift
    테스트를 위해 임시로 변경한 내용 수정

--- ChioskApp/ChioskApp/App/SceneDelegate.swift
+++ ChioskApp/ChioskApp/App/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         
         //window에게 루트 뷰 컨르롤러 지정.
-        window.rootViewController = OrderSummaryView()
+        window.rootViewController = MainViewController()
         //이 메서드를 반드시 작성해줘야만 윈도우가 활성화 됨
         window.makeKeyAndVisible()
         
